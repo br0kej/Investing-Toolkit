d25b5dd00ba6c7f32e39edcfd55ae64e36223b15
Br0kej <br0kej@protonmail.com>
2019-12-28 22:48:46 +0000

    Creating collector class tests & fixing bugs

--- investingtoolkit/collectors/collectors.py
+++ investingtoolkit/collectors/collectors.py
@@ -6,8 +6,7 @@ from bs4 import BeautifulSoup
 
 class Collector:
 
-    def __init__(self, target_url, html_element='table', output_directory):
-        self.results = list()
+    def __init__(self, target_url, output_directory, html_element='table'):
         self.timestamp = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
         self.target_url = target_url
         self.html_element = html_element
@@ -30,17 +29,15 @@ class Collector:
     def extract_target_html_object(self):
         self.extracted_html_object = self.parsed_html(self.html_element)
 
-    def output_to_json(self, output_directory, filename, results):
-        output_path = output_directory + filename + str(self.timestamp) + '.json'
-        with open(output_path) as file:
+    def output_to_json(self, filename, results):
+        output_path = self.output_directory + '/' + filename + str(self.timestamp) + '.json'
+        with open(output_path, 'w') as file:
             json.dump(results, file)
 
-TODO: Implement
+#TODO: Implement FTSE100 specific Wikipedia collectors
 class FTSE100(Collector):
     pass
 
-#    def iterate_extracted_html_object(self, element_position, sub_element):
-#        for row in self.extract_target_html_object[element_position].findall(sub_element)[1:-1]:
-
+#TODO: Implement FTSE250 specific Wikipedia collectors
 class FTSE250(Collector):
     pass
---
+++ tests/collectors_test.py
@@ -0,0 +1,38 @@
+from investingtoolkit.collectors.collectors import Collector
+import os
+import datetime
+
+x = Collector("https://en.wikipedia.org/wiki/FTSE_250_Index", "test_output")
+
+def test_timestamp_format():
+    assert datetime.datetime.strptime(x.timestamp, '%Y-%m-%d')
+
+def test_target_url():
+    assert x.target_url == "https://en.wikipedia.org/wiki/FTSE_250_Index"
+
+def test_output_directory():
+    assert x.output_directory == 'test_output'
+
+def test_html_element():
+    assert x.html_element == "table"
+
+def test_check_output_directory():
+    assert os.path.exists(x.output_directory)
+
+#TODO: Create test using Responses Library - https://pypi.org/project/responses/
+def test_make_requests():
+    pass
+
+#TODO: Look into how to create test this function
+def test_parse_html():
+    pass
+
+#TODO: Creat test for  this function
+def test_extract_target_html_object():
+    pass
+
+#TODO: Create test for this function
+def test_output_to_json():
+    results = [{'test1':'123'}]
+    x.output_to_json('test', results)
+    assert os.path.exists('test_output/test' + x.timestamp + '.json')
